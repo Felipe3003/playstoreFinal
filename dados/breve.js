1978bd73da0bfb004c136349d18b951c2fa54beb
unknown <you@example.com>
2022-04-26 09:55:55 -0300

    lancamentos

---
+++ dados/breve.js
@@ -0,0 +1,37 @@
+const breve =[
+
+    {
+        id:'01',
+        nome:'Fifa 23',
+        data:'Em breve',
+        imagem:'fifa.png'
+
+    },
+    {
+        id:'02',
+        nome:'Clash Royale',
+        data:'Em breve',
+        imagem:'clash.png'
+    },
+    {
+        id:'03',
+        nome:'Smite',
+        data:'Em breve',
+        imagem:'smite.png'
+    },
+    {
+        id:'04',
+        nome:'Clash Mini',
+        data:'Em breve',
+        imagem:'clashMini.jpg'
+    },
+    {
+        id:'05',
+        nome:'jogo do foguinho',
+        data:'Em breve',
+        imagem:'jogofogo.png'
+    }
+
+
+]
+export default breve;
---
+++ dados/dadosJogos.js
@@ -0,0 +1,109 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome : "Subway Surfers",
+        valor : "Gratuito",
+        imagem : 'subway.jpg'
+    },
+    {
+        id : "02",
+        nome : "Yugioh Master Duel",
+        valor : "Gratuito",
+        imagem : "yugi.jpg"
+    },
+    {
+        id : "03",
+        nome : "fortnite",
+        valor : "Gratuito",
+        imagem : "fortnite.png"
+    },
+    {
+        id : "04",
+        nome : "Genshin Impact",
+        valor : "Gratuito",
+        imagem : "genshin.png"
+    },
+    {
+        id : "05",
+        nome : "free fire",
+        valor : "Gratuito",
+        imagem : "free.png"
+    },
+    {
+        id : "06",
+        nome : "Clash of Clans",
+        valor : "Gratuito",
+        imagem : "cc.png"
+    },
+    {
+        id : "07",
+        nome : "ONE PIECE Bounty Rush",
+        valor : "Gratuito",
+        imagem : "OPBR.png"
+    },
+    {
+        id : "08",
+        nome : "Minecraft",
+        valor : "37.99",
+        imagem : "minecraft.png"
+    },
+    {
+        id : "09",
+        nome : "Avakin Life",
+        valor : "Gratuito",
+        imagem : "avakin.jpg"
+    },
+    {
+        id : "10",
+        nome : "ONE PIECE: Fighting Path!",
+        valor : "Gratuito",
+        imagem : "OPFP.png"
+    },
+    {
+        id : "11",
+        nome : "World War Heroes",
+        valor : "Gratuito",
+        imagem : "world.png"
+    },
+    {
+        id : "12",
+        nome : "Pokémon Masters EX",
+        valor : "Gratuito",
+        imagem : "master.png"
+    },
+    {
+        id : "13",
+        nome : "Roblox",
+        valor : "Gratuito",
+        imagem : "roblox.png"
+    },
+    {
+        id : "14",
+        nome : "Slither.io",
+        valor : "Gratuito",
+        imagem : "slither.jpg"
+    },
+    {
+        id : "15",
+        nome : "ONE PIECE: Treasure Cruise",
+        valor : "Gratuito",
+        imagem : "OPTC.png"
+    },
+    {
+        id : "16",
+        nome : "Dragon City",
+        valor : "Gratuito",
+        imagem : "dragon.png"
+    },
+    {
+        id : "16",
+        nome : "Candy Crush Saga",
+        valor : "Gratuito",
+        imagem : "candy.png"
+    },
+
+
+        
+]
+
+export default JOGOS;
